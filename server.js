83f16c2dd520bf60fccc9d3880c3cf024cb97bda
Marcelo Sanchez <39056094+msan72@users.noreply.github.com>
2022-01-20 16:48:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/crmAppFrontEnd'));
+app.use(express.static(__dirname + '/dist/customers-management'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/crmAppFrontEnd/index.html'));});
+'/dist/customers-management/index.html'));});
 app.listen(process.env.PORT || 8080);
