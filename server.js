3c5d7d26c9097bd7ff3517d7edfab32b93212567
Elias Sobarzo <elias.sobarzo@usach.cl>
2022-02-10 11:51:05 -0300

    Se modifica server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/customers-management'));
+app.use(express.static(__dirname + '/dist/citi-personal-crm'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/customers-management/index.html'));
+        '/dist/citi-personal-crm/index.html'));
 });
 app.listen(process.env.PORT || 8080);
