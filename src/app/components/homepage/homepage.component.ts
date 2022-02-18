a0ba3382bfcf33b1c6079244932d988ab3a34587
naaracastro <94947938+naaracastro@users.noreply.github.com>
2022-02-18 17:58:53 -0300

    merge con cambios Clau

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -23,6 +23,7 @@ export class HomepageComponent implements OnInit {
   }
 
   btnCreate(){this.router.navigate(["/create-customer/"]);}
+
 }
 
 
--- src/app/components/visualization/visualization.component.ts
+++ src/app/components/visualization/visualization.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from 'src/app/models/interface/customer.interface';
 import { CustomerService } from 'src/app/services/customer.service';
 import { Router } from '@angular/router';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-visualization',
@@ -28,23 +29,62 @@ export class VisualizationComponent implements OnInit {
     amount: '1500000',
    } */
 
-   customer: Customer;
+   customer: Customer = {
+     personalId: '',
+     name: '',
+     familyFirstName: '',
+     familySecondName: '',
+     birth: '',
+     countryId: 0,
+     countryName: '',
+     nationalityId: 0,
+     nacionalityName: '',
+     genderId: 0,
+     genderType: '',
+     email: '',
+     mobileNumberId: 0,
+     mobileNumber: 0,
+     mobileNumberCode: '',
+     fixNumberId: 0,
+     fixNumber: 0,
+     fixNumberCode: '',
+     addressCountryId: 0,
+     addressCountryName: '',
+     addressStreet: '',
+     addressNumber: 0,
+     addressComune: '',
+     addressPostalCode: '',
+     addressCity: '',
+     addressAditional: '',
+     income: 0,
+     currencyId: 0,
+     currencyName: '',
+     tipeOfClient: 0
+   };
+   isCustomer: number = 0;
    
 
   constructor(
     private customerService: CustomerService,
     private router: Router,
+    private msgService: MessageService,
+    
     ) {}
 
 
 
-  ngOnInit(): void {
-    this.customerService.getCustomer(1)
+  ngOnInit() {
+    this.subscription = this.msgService.getMessage().subscribe(
+      msg => console.log(msg)
+      
+    )
+    this.customerService.getCustomer(28)
     .subscribe(
       res => {
         if(res){
           console.log(res.data);
           this.customer = res.data;
+          this.isCustomer = res.data.tipeOfClient;
         }}
     )
 
@@ -52,10 +92,17 @@ export class VisualizationComponent implements OnInit {
   }
 
   btnHome() {
-    console.log("entra al home");
   this.router.navigate(
     ['/homepage/']
   );
   }
+
+  btnCreate() {
+  this.router.navigate(
+    ['/create-customer/']
+  );
+  }
+
+
 
 }
